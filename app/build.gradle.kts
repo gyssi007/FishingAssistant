1b6a37f75c8886fc5ab3e8650fd755183b52f930
gyssi007 <78358246+gyssi007@users.noreply.github.com>
2026-05-17 00:33:19 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation 'com.squareup.okhttp3:okhttp:4.12.0'
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation 'com.google.code.gson:gson:2.10.1'
 }
